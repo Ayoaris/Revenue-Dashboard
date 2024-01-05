9fac768ae4467ff2db85205124102aa9dbe49b34
Samuel Ayodeji <ayodejisoyebo@gmail.com>
2024-01-05 10:29:41 +0100

    Data file

--- data/index.ts
+++ data/index.ts
@@ -1,14 +1,17 @@
+// Transaction Type
 export const TRANSACTION_TYPE = [
-    { name: "Store Transactions", value: "Store Transactions" },
-    { name: "Get Tipped", value: "Get Tipped" },
-    { name: "Withdrawal", value: "Withdrawal" },
-    { name: "Chargebacks", value: "Chargebacks" },
-    { name: "Cashbacks", value: "Cashbacks" },
-    { name: "Refer & Earn", value: "Refer & Earn" },
+  { name: "Store Transactions", value: "Store Transactions" },
+  { name: "Get Tipped", value: "Get Tipped" },
+  { name: "Withdrawal", value: "Withdrawal" },
+  { name: "Chargebacks", value: "Chargebacks" },
+  { name: "Cashbacks", value: "Cashbacks" },
+  { name: "Refer & Earn", value: "Refer & Earn" },
 ];
 
+// Transaction Status
+
 export const TRANSACTION_STATUS = [
-    { name: "Successful", value: "Successful" },
-    { name: "Pending", value: "pending" },
-    { name: "Failed", value: "failed" },
+  { name: "Successful", value: "Successful" },
+  { name: "Pending", value: "pending" },
+  { name: "Failed", value: "failed" },
 ];
